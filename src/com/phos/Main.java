49b2090765981bd5d9e3adb2b7b3b0cc7a959c12
folzieds <aosu@teamapt.com>
2019-08-06 09:28:51 +0100

    added conditions to handle some exceptions

--- src/com/phos/Main.java
+++ src/com/phos/Main.java
@@ -9,23 +9,45 @@ public class Main {
         final byte MONTHS_IN_YEARS = 12;
         final byte PERCENT = 100;
 
-        //principal
-        System.out.print("Principal: ");
         Scanner scanner = new Scanner(System.in);
-        int principal = scanner.nextInt();
+        // initializers
+        int principal = 0;
+        float rate = 0.0F;
+        byte period =0;
+
+        //principal
+        while(true){
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter an amount between 1000 and 1,000,000");
+        }
 
         // annual interest rate
-        System.out.print("Annual Interest Rate: ");
-        Double rate = scanner.nextDouble();
+        while  (true){
+            System.out.print("Annual Interest Rate: ");
+            rate = scanner.nextFloat();
+            if (rate >= 1 && rate <= 30)
+                break;
+            System.out.println("Enter a value greater than 0 and less  than 31");
+        }
+
 
         // calculates the monthly interest rate
-        Double monthlyRate = rate / (MONTHS_IN_YEARS * PERCENT);
+        Float monthlyRate = rate / (MONTHS_IN_YEARS * PERCENT);
 
         // Period in years
-        System.out.print("Period (years): ");
-        Double period = scanner.nextDouble();
+        while(true){
+            System.out.print("Period (years): ");
+            period = scanner.nextByte();
+            if(period >= 1 && period <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30");
+        }
+
         // calculates the period in months
-        Double monthlyPeriod = period * MONTHS_IN_YEARS;
+        int monthlyPeriod = period * MONTHS_IN_YEARS;
 
         // calculates the (1 + r) ^ n
         Double power = Math.pow((1 + monthlyRate),monthlyPeriod);
